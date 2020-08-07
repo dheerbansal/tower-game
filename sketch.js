3f0a1acf03343c4e01a4c24a9b017907cf685bf0
dheerbansal <67144062+dheerbansal@users.noreply.github.com>
2020-08-07 09:06:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var block1
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function setup() {
+  engine = Engine.create();
+	world = engine.world;
+  createCanvas(1200,800);
+ 
+  block1 = new Block(100,200,this.width, this.height);
+  block1.shapeColor = "white";
+  block1.scale = 0.09;
+
+  Engine.run(engine);
+}
+
+function draw() {
+  Engine.update(engine)
+  background(0);  
+  block1.display();
+
+  drawSprites();
+}
